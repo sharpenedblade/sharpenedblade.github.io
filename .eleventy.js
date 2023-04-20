43f045188934dd9df2ab7ddc244314541d989403
sharpenedblade <sharpenedblade@proton.me>
2023-04-19 23:52:38 -0700

    Support light mode.

--- .eleventy.js
+++ .eleventy.js
@@ -3,18 +3,7 @@ const sass = require("sass");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("styles");
 
     eleventyConfig.addTemplateFormats("scss");
-
-    eleventyConfig.addExtension("scss", {
-        outputFileExtension: "css",
-
-        compile: async function(inputContent) {
-            let result = sass.compileString(inputContent);
-
-            return async (data) => {
-                return result.css;
-            };
-        }
-    });
 };
