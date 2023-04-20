432392b78c2b19fe67d1abd4a74ad9bdf99f39c7
sharpenedblade <sharpenedblade@proton.me>
2023-04-19 23:52:38 -0700

    sccs and retheme.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,19 @@
+const sass = require("sass");
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("assets");
+
+    eleventyConfig.addTemplateFormats("scss");
+
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+
+        compile: async function(inputContent) {
+            let result = sass.compileString(inputContent);
+
+            return async (data) => {
+                return result.css;
+            };
+        }
+    });
 };
