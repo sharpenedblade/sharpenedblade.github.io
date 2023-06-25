53a1fe5980c46fb84b0b32fc2620c20375850903
sharpenedblade <sharpenedblade@proton.me>
2023-06-25 16:51:37 +0530

    feat: Initial tailwindcss support
    This adds tailwindcss as a dependency and imports it into the style
    sheet. The actual stylesheets have not been ported to tailwindcss
    classes yet.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body>
+            <body className="bg-nord0 text-nord4">
                 <Main />
                 <NextScript />
             </body>
