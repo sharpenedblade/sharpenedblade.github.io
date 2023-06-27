fb77e7c131be7044f6c51c70b7563c33cee8aba5
sharpenedblade <sharpenedblade@proton.me>
2023-06-27 17:07:46 +0530

    articleLink.js: Port styling to tailwindcss

--- src/components/articleLink.js
+++ src/components/articleLink.js
@@ -1,20 +1,17 @@
 // Copyright (c) 2022-2023 Sharpened Blade
 
-import styles from "@/styles/articleLink.module.css";
 import Link from "next/link";
 
 export default function ArticleLink({ path, name, description, date, id }) {
     return (
-        <article className={styles.article} key={id}>
-            <div>
-                <time dateTime={date} className={styles.date}>
-                    {date}
-                </time>{" "}
-                <Link href={path} className={styles.link}>
-                    {name}
-                </Link>
-            </div>
-            <p className={styles.description}>{description}</p>
+        <article className="my-8 space-x-2" key={id}>
+            <time dateTime={date} className="text-nord4 text-sm">
+                {date}
+            </time>
+            <Link href={path} className="text-xl text-nord8 font-bold hover:underline">
+                {name}
+            </Link>
+            <p className="ml-4">{description}</p>
         </article>
     );
 }
