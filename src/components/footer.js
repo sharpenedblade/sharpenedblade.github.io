6eeb23025405490dfed37f7b29a53ea15af77bfc
sharpenedblade <sharpenedblade@proton.me>
2023-08-14 22:27:45 -0700

    Port all components to use link.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,15 @@
 // Copyright (c) 2022-2023 Sharpened Blade
 
+import Link from "@/components/link.js";
+
 export default function Footer() {
     return (
-        <footer className="fg-nord6 min-w-full bg-nord1 py-0.5 text-center text-sm">
-            <p className="bg-nord1 text-nord4">
-                <a
-                    className="font-semibold text-nord8 hover:underline"
-                    href="https://github.com/sharpenedblade/sharpenedblade.github.io"
-                >
-                    Source code
-                </a>{" "}
-                for this website. If you spot a mistake you can email me or open
-                an issue with the source code repo
-            </p>
+        <footer className="fg-nord6 min-w-full bg-nord1 py-0.5 text-center text-sm text-nord4">
+            <Link href="https://github.com/sharpenedblade/sharpenedblade.github.io">
+                Source code
+            </Link>{" "}
+            for this website. If you spot a mistake you can email me or open an
+            issue with the source code repo
         </footer>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,14 @@
 // Copyright (c) 2022-2023 Sharpened Blade
 
-import Link from "next/link";
+import { InternalLink as InternalLink } from "@/components/link.js";
 
 export default function Header() {
     return (
         <header className="min-w-full space-x-2 bg-nord1 py-2 text-center text-base text-nord6">
             <nobr>Sharpened Blade&apos;s Website</nobr>
             <nav className="inline-block space-x-2">
-                <Link
-                    className="font-semibold text-nord8 hover:underline"
-                    href="/"
-                >
-                    Home
-                </Link>
-                <Link
-                    className="font-semibold text-nord8 hover:underline"
-                    href="/about"
-                >
-                    About
-                </Link>
+                <InternalLink href="/">Home</InternalLink>
+                <InternalLink href="/about">About</InternalLink>
             </nav>
         </header>
     );
