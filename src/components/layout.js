1dc7fd548f8d3520daae5ead8e5d1f91bacb561a
sharpenedblade <sharpenedblade@proton.me>
2023-08-18 15:40:17 -0700

    Remove </Footer> from all pages
    The footer was unnecessary since the information was moved to the
    about page.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 // Copyright (c) 2022-2023 Sharpened Blade
 
-import Footer from "@/components/footer.js";
 import Header from "@/components/header.js";
 
 export default function Layout({ children }) {
@@ -8,7 +7,6 @@ export default function Layout({ children }) {
         <div className="flex min-h-screen flex-col items-center bg-zinc-100 dark:bg-zinc-900">
             <Header></Header>
             <main className="flex flex-grow flex-col">{children}</main>
-            <Footer></Footer>
         </div>
     );
 }
