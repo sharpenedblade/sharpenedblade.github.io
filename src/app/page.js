06bf6303767da9950b29fc9a4a025b447af74570
sharpenedblade <sharpenedblade@proton.me>
2023-12-18 23:23:41 -0800

    Add link to RSS and JSON feeds

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,16 @@ export default async function Index() {
     const allArticles = await getAllArticles();
     return (
         <>
+            <p className="text-small my-4">
+                <a href="feed/rss" className="font-semibold underline">
+                    RSS
+                </a>{" "}
+                and{" "}
+                <a href="feed/json" className="font-semibold underline">
+                    JSON
+                </a>{" "}
+                feeds are supported.
+            </p>
             {allArticles.map(({ title, date, description, id }) => (
                 <ArticleLink
                     path={"/articles/" + id}
