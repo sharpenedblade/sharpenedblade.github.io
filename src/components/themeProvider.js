01ccd6c07a0699e495cacbe3e0bf984d5f2f1eba
sharpenedblade <sharpenedblade@proton.me>
2024-02-14 18:03:37 -0800

    Initial SvelteKit scaffolding

--- src/components/themeProvider.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ThemeProvider as ClientThemeProvider } from "next-themes";
-
-export default function ThemeProvider({ children }) {
-    return (
-        <ClientThemeProvider attribute="class">{children}</ClientThemeProvider>
-    );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./src/app/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-        "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
-        "content/**/*.{md,mdx}",
-    ],
-    darkMode: "class",
+    content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
         extend: {},
-        fontFamily: {
-            sans: ["var(--font-inter)", "sans-serif"],
-            mono: ["var(--font-iosevka)", "monospace"],
-        },
     },
-    plugins: [
-        require("@tailwindcss/typography"),
-        require("@catppuccin/tailwindcss")({
-            prefix: "ctp",
-        }),
-    ],
+    plugins: [require("@tailwindcss/typography")],
 };
