147cfd513df1a7bf718aa74bc57c8f6835d69475
sharpenedblade <sharpenedblade@proton.me>
2024-02-14 18:03:37 -0800

    Initial SvelteKit scaffolding

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-    reactStrictMode: true,
-    output: "export",
-    trailingSlash: true,
-    sassOptions: {
-        includePaths: ["./node_modules"],
-    },
-};
